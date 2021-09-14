30fd0a79a897e07b5ce9cc302ac2b7c573ec53f7
TimCejka <tcejka@uchicago.edu>
2021-09-14 10:34:52 -0500

    drafted collatz
    Finished collatz conjecture
    
    Finished acronym

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,2 +1,37 @@
-def abbreviate(words):
-    pass
+import re
+
+def abbreviate(name):
+    #name=input("Enter name: ")
+    name_split= re.split("\s|-", name)
+
+    acronym="" 
+
+    for i in name_split:
+        if len(i) == 0:
+            continue
+        char = i[0]
+        if char.isalpha():
+            acronym += char.upper()
+        else:
+            count = 0
+            for c in i:
+                if c.isalpha():
+                    break
+                else:
+                    count += 1
+            if count >= len(i):
+                continue
+            else:
+                char = i[count]
+                acronym += char.upper()
+
+    return(acronym)
+
+print(abbreviate("Something - I made up from thin air"))
+print(abbreviate("The Road _Not_ Taken"))
+print(abbreviate("Complementary metal-oxide semiconductor"))
+
+
+
+
+
