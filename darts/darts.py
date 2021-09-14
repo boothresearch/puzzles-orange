c4a10593174da4f638fc05f2f52acd8b1b2498de
pspahwa <pspahwa@ucsd.edu>
2021-09-14 10:07:56 -0500

    Did first 2.

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,9 @@
 def score(x, y):
-    pass
+    distance = (x**2 + y**2)**0.5
+    if distance <= 10 and distance > 5:
+        return 1
+    elif distance <= 5 and distance > 1:
+        return 5
+    elif distance <= 1:
+        return 10
+    return 0 
