c67b862bf0b14c67447dc9057c33a95840afe7a5
TimCejka <tcejka@uchicago.edu>
2021-09-14 10:34:52 -0500

    drafted collatz

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,22 @@
-def steps(number):
-    pass
+def steps(n):
+    if(n<=0):
+        return(ValueError)
+        print("n must be positive")
+    else:
+        s=0
+        while(n>1) :
+            s=s+1
+            # if n is odd:
+            if (n % 2) != 0:
+                n = 3*n + 1
+            # else n is even:
+            else:
+                n = n/2
+        return(s)
+
+#n = int(input('Enter n: '))
+#print('Number of steps: ', end=steps(n))
+#steps(n)
+
+
+
